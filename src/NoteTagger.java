58a44ec683371047a19714a9a7201ad4bc3fece5
maks ggs <maksggs111@gmail.com>
2026-03-02 10:23:06 +0700

    Сростил с NoteTagger

--- src/NoteTagger.java
+++ src/NoteTagger.java
@@ -42,31 +42,3 @@ public class NoteTagger {
         return false;
     }
 }
- 
-        import java.util.List;
-import java.util.stream.Collectors;
-
-/**
- * Фильтрация заметок по тегам
- */
-public class NoteFilter {
-    private NoteStorage storage;
-
-    public NoteFilter() {
-        this.storage = NoteStorage.getInstance();
-    }
-
-    public List<Note> filterByTag(String tag) {
-        return storage.getAllNotes().stream()
-                .filter(n -> !n.isArchived())
-                .filter(n -> n.getTags().contains(tag))
-                .collect(Collectors.toList());
-    }
-
-    public void printFilteredByTag(String tag) {
-        List<Note> filtered = filterByTag(tag);
-        System.out.println("\n=== ЗАМЕТКИ С ТЕГОМ '" + tag + "' ===");
-        filtered.forEach(n ->
-                System.out.println(n.getId() + ": " + n.getTitle()));
-    }
-}
